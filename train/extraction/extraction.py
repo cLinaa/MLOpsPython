1ad07efa30d6ba7bd90fb7aaa30f75784f3b6aaf
cLinaa <101574816+cLinaa@users.noreply.github.com>
2023-02-27 11:18:50 +0100

    test

--- train/extraction/extraction.py
+++ train/extraction/extraction.py
@@ -14,21 +14,27 @@ def convert_pixmap_to_rgb(pixmap) -> Pixmap:
 pdfs_directory_path = ".\dataset-cats-dogs-others"
 images_directory_path = ".\extracted_images"
 
-Path(images_directory_path).mkdir(parents=True, exist_ok=True)
-files = [p for p in Path(pdfs_directory_path).iterdir() if p.is_file()]
-for path in files:
-    with open(path, "rb") as file_stream:
-        with fitz.open(stream=file_stream.read(), filetype="pdf") as d:
-            file_images = []
-            nb = len(d) - 1
-            for i in range(nb):
-                page = d[i]
-                imgs = d.get_page_images(i)
-                nb_imgs = len(imgs)
-                for j, img in enumerate(imgs):
-                    xref = img[0]
-                    pix = fitz.Pixmap(d, xref)
-                    bytes = BytesIO(convert_pixmap_to_rgb(pix).tobytes())
-                    fn = path.stem + "_page" + str(i) + "_index" + str(j) + ".png"
-                    with open(images_directory_path + "\" + fn, "wb") as f:
-                        f.write(bytes.getbuffer())
+def extract_images_from_pdfs(pdfs_directory_path:str,images_directory_path:str):
+    Path(images_directory_path).mkdir(parents=True, exist_ok=True)
+    files = [p for p in Path(pdfs_directory_path).iterdir() if p.is_file()]
+    for path in files:
+        with open(path, "rb") as file_stream:
+            with fitz.open(stream=file_stream.read(), filetype="pdf") as d:
+                file_images = []
+                nb = len(d) - 1
+                for i in range(nb):
+                    page = d[i]
+                    imgs = d.get_page_images(i)
+                    nb_imgs = len(imgs)
+                    for j, img in enumerate(imgs):
+                        xref = img[0]
+                        pix = fitz.Pixmap(d, xref)
+                        bytes = BytesIO(convert_pixmap_to_rgb(pix).tobytes())
+                        filename = path.stem + "_page" + str(i) + "_index" + str(j) + ".png"
+                        path_filename = "{0}\\{1}".format(images_directory_path, filename)
+                        file_images.append(path_filename)
+                        with open(path_filename, "wb") as f:
+                            f.write(bytes.getbuffer())
+                return file_images
+
+#extract_images_from_pdfs(pdfs_directory_path,images_directory_path)
--- train/extraction/tests/extraction_test.py
+++ train/extraction/tests/extraction_test.py
@@ -1,11 +1,24 @@
 import unittest
 
+from train.extraction.extraction import extract_images_from_pdfs
+from pathlib import Path
+
+BASE_PATH = Path(__file__).resolve().parent
+output_directory = BASE_PATH / "output"
+input_directory = BASE_PATH / "input"
+
 class TestStringMethods(unittest.TestCase):
 
     def test_upper(self):
         value = 'foo'.upper()
         expected = 'FOO'
         self.assertEqual(value, expected)
+
+
+    def test_something(self):
+        images=extract_images_from_pdfs(str(input_directory, str(output_directory)))
+        expected_result = ['0a0e5d35-ef01-4239-af60-81f2357a6ab9.pdf']
+        self.assertEqual(expected_result, images)
 
 if __name__ == '__main__':
     unittest.main()
